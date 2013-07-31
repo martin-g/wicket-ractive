5767ef371b0860e2facf0b23b2fc971a9a20a175
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-07-31 10:02:18 +0200

    Return only the JSON for the changed inner object of the RactiveObject

--- src/main/java/com/mycompany/ractive/RactiveRequestHandler.java
+++ src/main/java/com/mycompany/ractive/RactiveRequestHandler.java
@@ -1,9 +1,15 @@
 package com.mycompany.ractive;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 import com.mycompany.json.Json;
 import org.apache.wicket.Component;
 import org.apache.wicket.Page;
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AjaxRequestHandler;
+import org.apache.wicket.ajax.json.JSONException;
+import org.apache.wicket.ajax.json.JSONObject;
 
 /**
  *
@@ -26,24 +32,63 @@ public class RactiveRequestHandler extends AjaxRequestHandler
 		for (Component component : components)
 		{
 			boolean processed = false;
+
 			Component cursor = component;
+			Deque<String> paths = new LinkedList<String>();
 			while (cursor instanceof Page == false)
 			{
 				Object modelObject = cursor.getDefaultModelObject();
-				if (modelObject instanceof RactiveObject) {
+				if (modelObject instanceof RactiveObject)
+				{
+					CharSequence childPath = getChildPath(paths);
+					JSONObject json = Json.toJsonObject(cursor);
+					JSONObject childJson;
+					try
+					{
+						childJson = getChildJson(json, paths);
+					} catch (JSONException e)
+					{
+						throw new WicketRuntimeException(e);
+					}
 
-					String json = Json.stringify(cursor);
-
-					appendJavaScript(String.format("Wicket.Ractive['%s'].set('w', %s)", cursor.getMarkupId(), json));
+					appendJavaScript(String.format("Wicket.Ractive['%s'].set('w%s', %s)", cursor.getMarkupId(), childPath, childJson));
 					processed = true;
 					break;
 				}
+				paths.addFirst(cursor.getId());
 				cursor = cursor.getParent();
 			}
 			if (processed == false)
 			{
 				super.add(component);
 			}
+		}
+	}
+
+	private CharSequence getChildPath(Deque<String> paths)
+	{
+		StringBuilder result = new StringBuilder();
+		if (paths.isEmpty() == false)
+		{
+			for (String path : paths)
+			{
+				result.append('.').append(path);
+			}
+		}
+
+		return result;
+	}
+
+	private JSONObject getChildJson(JSONObject json, Deque<String> paths) throws JSONException
+	{
+		if (paths.isEmpty())
+		{
+			return json;
+		}
+		else
+		{
+			String path = paths.removeFirst();
+			return getChildJson(json.getJSONObject(path), paths);
 		}
 	}
 }
